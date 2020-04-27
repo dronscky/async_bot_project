dc2a1f725aaf7aff7106b0fe1b4b9578b963a1fa
dronscky <dronscky@mail.ru>
2020-04-27 22:27:20 +0500

    Add proxy

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from aiogram.types import Message
 
 from config import BOT_TOKEN
 
+PROXY_URL="http://54.37.14.65:3132"
+
 loop = asyncio.get_event_loop()
-bot = Bot(BOT_TOKEN, parse_mode="HTML")
+bot = Bot(BOT_TOKEN, proxy=PROXY_URL, parse_mode="HTML")
 dp = Dispatcher(bot, loop=loop)
 
-@dp.mesage_handler()
+@dp.message_handler()
 async def echo(message: Message):
 	text = f"Вы написали {message.text}"
 	await bot.send_message(chat_id=message.from_user.id, text=text)
