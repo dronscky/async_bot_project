c6ae17054430a2a4a640369d63817de1c848848c
dronscky <dronscky@mail.ru>
2020-05-25 17:17:19 +0500

    voting

--- house.py
+++
@@ -1,8 +0,0 @@
-class House:
-'''address, abbrev, vote, house_population, uik_population'''
-	def __init__(self, abbrev):
-		self.abbrev = abbrev
-		self._houses = self._load_houses()
-
-	def _load_houses():
-		pass
---
+++ voting.py
@@ -0,0 +1,11 @@
+import db
+
+def add_vote(message):
+	abbrev, vote = parse_mesg(message)
+	
+	
+	отредактировать бд о количестве голосов
+	
+
+def parse_mesg(message):
+	pass
